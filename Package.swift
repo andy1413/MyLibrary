0c894c79ceb0554cd06731621c55de64dd2af5a1
王方帅 <wangfangshuai@apusapps.com>
2020-08-31 16:38:59 +0800

    --

--- Package.swift
+++ Package.swift
@@ -20,10 +20,9 @@ let package = Package(
         // Targets can depend on other targets in this package, and on products in packages this package depends on.
         .target(
             name: "MyLibrary",
-            dependencies: ["SDWebImage"]
+            dependencies: ["SDWebImage"],
+            exclude: [],
+            publicHeadersPath: "include"
         ),
-        .testTarget(
-            name: "MyLibraryTests",
-            dependencies: ["MyLibrary"]),
     ]
 )
